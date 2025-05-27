8b2dfbac29c0dd044e1c34f41f93d69440cdf0ce
onulee <onulee@naver.com>
2025-05-27 17:13:09 +0900

    데이터전송

--- w0527/w03/students/urls.py
+++ w0527/w03/students/urls.py
@@ -5,4 +5,7 @@ app_name = 'students'
 urlpatterns = [
     path('list/', views.list,name='list' ),
     path('view/', views.view,name='view' ),
+    path('write/', views.write,name='write' ), # 페이지 열기
+    path('result/', views.result,name='result' ), # 결과확인
+    path('send/<str:name>/<int:age>/', views.send,name='send' ), # 결과확인
 ]
--- w0527/w03/students/views.py
+++ w0527/w03/students/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
 
+
+def send(request,name,age):
+    
+    print("전달받은 값 : ",name,age)
+    context = {"name":name,"age":age}
+    return render(request,'students/send.html',context)
+
+
+
+def write(request):
+    return render(request,'students/write.html')
+
+def result(request):
+    # post방식 - name,kor,eng,hobby
+    name = request.POST.get('name')
+    kor = request.POST.get('kor')
+    eng = request.POST.get('eng')
+    total = int(kor)+int(eng)  # 타입변경 -> 합계계산
+    hobbys = request.POST.getlist('hobby')
+    print("이름 : ",name)
+    context = {"name":name,"kor":kor,"eng":eng,"total":total,"hobby":hobbys}
+    return render(request,'students/result.html',context)
+
 def view(request):
     # get방식
     name = request.GET.get('name')
