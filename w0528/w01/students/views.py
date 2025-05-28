cd6bd41c8c1ed229acb05ad9786103e7b9267691
onulee <onulee@naver.com>
2025-05-28 12:18:58 +0900

    리스트 출력,정렬

--- w0528/w01/students/views.py
+++ w0528/w01/students/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponseRedirect
+from students.models import Student  #Student테이블
 
 # 학생정보 등록
 def write(request):
@@ -10,15 +11,23 @@ def write(request):
         age = request.POST.get('age')
         gender = request.POST.get('gender')
         print("입력된 정보 : ",name,major,grade,age,gender)
-        print("request method : ",request.method)
-        return redirect('/')
+        ## DB저장
+        ## 1. 데이터.save() / 2. create() 
+        Student(name=name,major=major,grade=grade,age=age,gender=gender).save()
+        print("Student 객체 저장")
+        return redirect('/students/list/')
         
     else: # GET방식으로 들어올때
-        print("request : ",request)
-        print("request get : ",request.GET)
         print("request method : ",request.method)
         return render(request,'students/write.html')
 
 # 학생정보 리스트
 def list(request):
-    return render(request,'students/list.html')
+    # DB검색 
+    # objects.all():전체가져오기, objects.get(): 해당되는것가져오기
+    # objects.filter(): 검색기능
+    # objects.order_by() : 정렬, -정렬
+    # qs = Student.objects.all() # 전체가져오기
+    qs = Student.objects.order_by('-id') # 순차정렬
+    context = {'list':qs,'count':100,'id':'aaa'} # 딕셔너리 타입으로 전달
+    return render(request,'students/list.html',context)
