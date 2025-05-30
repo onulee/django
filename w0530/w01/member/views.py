228aa64c21ab4b52d2ffaca6d4585cc80dbea55b
onulee <onulee@naver.com>
2025-05-30 14:34:06 +0900

    로그인

--- w0530/w01/member/views.py
+++ w0530/w01/member/views.py
@@ -9,17 +9,16 @@ def login(request):
         id = request.POST.get('id')
         pw = request.POST.get('pw')
         print("아이디, 패스워드 : ",id, pw)
-        
         # id,pw가 있는지 확인
         try:
             qs = Member.objects.get(id=id)
             if qs.pw == pw:
                 request.session['session_id'] = id  # session할당
-                txt = 1 # 성공
+                txt = 1
             else:
-                txt = -1 # 아이디는 존재, 패스워드가 일치하지 않습니다. 다시 입력하세요.    
+                txt = -1
         except:
-            txt = 0 # 아이디가 없습니다. 회원가입을 해야 로그인이 가능합니다.
+            txt = 0
         
         # try:
         #     qs = Member.objects.get(id=id,pw=pw)
@@ -27,7 +26,6 @@ def login(request):
         #     txt = 1 # 성공
         # except:
         #     txt = 0 # 실패
-        
         context = {'msg':txt}
         return render(request,'member/login.html',context)
         # return redirect('/')
