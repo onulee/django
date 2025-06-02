2c488f2ba8b33b280d60ba788006afffa8f13450
onulee <onulee@naver.com>
2025-06-02 16:46:17 +0900

    게시판리스트, 게시판쓰기

--- w0602/w01/board/views.py
+++ w0602/w01/board/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from board.models import Board
 
 # 쓰기페이지, 쓰기 저장
@@ -10,14 +10,18 @@ def write(request):
         btitle = request.POST.get('btitle')
         bcontent = request.POST.get('bcontent')
         bfile = request.POST.get('bfile')
+        # DB저장후 qs변수로 다시 리턴받음.
         qs = Board.objects.create(id=id,btitle=btitle,bcontent=bcontent)
         qs.bgroup = qs.bno
         qs.save()
         print('데이터 확인 : ',btitle,bcontent,bfile)
         print('데이터추가 : ',qs.bgroup,qs.bstep,bfile)
-        return render(request,'board/write.html')
+        return redirect('board:list')
         
 
 # 게시판리스트
 def list(request):
-    return render(request,'board/list.html')
+    # 모든 데이터 가져오기
+    qs = Board.objects.order_by('-bno')
+    context = {'list':qs}
+    return render(request,'board/list.html',context)
