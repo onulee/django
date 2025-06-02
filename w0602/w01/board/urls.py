3d374e8ee42ed486985187ddf613f3c227f89782
onulee <onulee@naver.com>
2025-06-02 18:15:39 +0900

    글수정저장

--- w0602/w01/board/urls.py
+++ w0602/w01/board/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 app_name='board'
 urlpatterns = [
-    path('list/', views.list, name='list' ), 
-    path('write/', views.write, name='write' ), 
+    path('', views.list, name='list' ),        # 게시판리스트
+    path('list/', views.list, name='list' ),   # 게시판리스트
+    path('write/', views.write, name='write' ),              # 글쓰기 
+    path('view/<int:bno>/', views.view, name='view' ),       # 글 상세보기
+    path('update/<int:bno>/', views.update, name='update' ), # 글수정
 ]
--- w0602/w01/board/views.py
+++ w0602/w01/board/views.py
@@ -1,11 +1,38 @@
 from django.shortcuts import render,redirect
 from board.models import Board
 
+# 글수정페이지, 글수정저장
+def update(request,bno):
+    if request.method == 'GET': #글수정페이지
+        qs = Board.objects.get(bno=bno)  #1개 게시글가져오기
+        context = {"board":qs}
+        return render(request,'board/update.html',context)
+    
+    elif request.method == "POST": # 글수정저장
+        # 변경된 게시글 가져오기 - 몇 게시글 수정이야?
+        btitle = request.POST.get('btitle')
+        bcontent = request.POST.get('bcontent')
+        # 1.변경할 게시글 가져오기 - 2.변경된 내용 게시글 저장
+        qs = Board.objects.get(bno=bno)
+        qs.btitle = btitle
+        qs.bcontent = bcontent
+        qs.save()
+        context = {'msg': 1}   # 글수정저장성공
+        return render(request,'board/update.html',context)
+
+
+# 상세보기
+def view(request,bno):
+    qs = Board.objects.get(bno=bno)
+    context = {'board':qs}
+    return render(request,'board/view.html',context)
+
+
 # 쓰기페이지, 쓰기 저장
 def write(request):
-    if request.method == 'GET':
+    if request.method == 'GET': # 글쓰기페이지
         return render(request,'board/write.html')
-    elif request.method == 'POST':
+    elif request.method == 'POST': # 글쓰기 저장
         id = 'aaa'
         btitle = request.POST.get('btitle')
         bcontent = request.POST.get('bcontent')
