c7b864b7b514c002bb1b7b50d9c8139393bb9dd2
onulee <onulee@naver.com>
2025-06-02 12:47:30 +0900

    섹션,쿠키 로그인부분

--- w0602/w01/member/views.py
+++ w0602/w01/member/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render,redirect
+from member.models import Member
+
+### 회원가입 03 - 가입완료
+def join03(request):
+    return render(request,'member/join03.html')
 
 ### 회원가입 02 - 회원가입페이지, 회원가입저장
 def join02(request):
@@ -6,12 +11,35 @@ def join02(request):
         return render(request,'member/join02.html')
     
     elif request.method == 'POST': #회원가입저장
-        return render(request,'member/join02.html')
-
+        id = request.POST.get('id')
+        pw = request.POST.get('pw')
+        name = request.POST.get('name')
+        nickName = request.POST.get('nickName')
+        f_tell = request.POST.get('f_tell')
+        m_tell = request.POST.get('m_tell')
+        l_tell = request.POST.get('l_tell')
+        tel = f"{f_tell}-{m_tell}-{l_tell}"
+        gender = request.POST.get('gender')
+        hobby = request.POST.getlist('hobby')
+        hobby = ','.join(hobby)
+        # db저장
+        Member(id=id,pw=pw,name=name,nickName=nickName,tel=tel,gender=gender,hobby=hobby).save()
+        
+        print("넘어온 데이터 : ",id,pw,name,nickName,tel,gender,hobby)
+        
+        return redirect('/member/join03/')
 
 ### 회원가입 01 - 동의페이지
 def join01(request):
     return render(request,'member/join01.html')
+
+#-----------------------------------------------------------------
+def logout(request):
+    # session 모두삭제
+    request.session.clear()
+    context = {'msg':-1}
+    return render(request,'member/login.html',context)
+
 
 ### login페이지연결, login확인
 def login(request):
@@ -27,8 +55,21 @@ def login(request):
         id = request.POST.get('id') #아이디
         pw = request.POST.get('pw') #패스워드
         idCheck = request.POST.get('idCheck') # 있을수도 있고, 없을수도 있고
+        
+        # 로그인체크
+        try:
+            qs = Member.objects.get(id=id,pw=pw)
+        except:
+            context = {'msg':0}
+            return render(request,'member/login.html',context)
+            
+        # session 저장
+        request.session['session_id'] = id
+        request.session['session_nickName'] = qs.nickName
+                
         # response 쿠키 저장
-        response = redirect('/')
+        context = {'msg':1}
+        response = render(request,'member/login.html',context)
         if idCheck != None: # idCheck값이 있으면 - max_age = 60초*60분*24시간*365일
             response.set_cookie('idCheck',id,max_age=60*60) #쿠키저장
         else:
