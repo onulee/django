c062cb4a66a5a65c78efc52588b0c43297d8bc4e
onulee <onulee@naver.com>
2025-06-02 16:19:56 +0900

    게시판 글쓰기

--- w0602/w01/board/views.py
+++ w0602/w01/board/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
+from board.models import Board
 
-# 글쓰기
+# 쓰기페이지, 쓰기 저장
 def write(request):
-    return render(request,'board/write.html')
+    if request.method == 'GET':
+        return render(request,'board/write.html')
+    elif request.method == 'POST':
+        id = 'aaa'
+        btitle = request.POST.get('btitle')
+        bcontent = request.POST.get('bcontent')
+        bfile = request.POST.get('bfile')
+        qs = Board.objects.create(id=id,btitle=btitle,bcontent=bcontent)
+        qs.bgroup = qs.bno
+        qs.save()
+        print('데이터 확인 : ',btitle,bcontent,bfile)
+        print('데이터추가 : ',qs.bgroup,qs.bstep,bfile)
+        return render(request,'board/write.html')
+        
 
 # 게시판리스트
 def list(request):
