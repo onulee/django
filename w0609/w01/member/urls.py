3fb6dd187321ed18ae792c415853434575b286dd
onulee <onulee@naver.com>
2025-06-09 12:22:32 +0900

    login부분

--- w0609/w01/member/urls.py
+++ w0609/w01/member/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('step01/', views.step01, name='step01'),
     path('step02/', views.step02, name='step02'),
     path('step03/', views.step03, name='step03'),
+    path('login/', views.login, name='login'),
 ]
--- w0609/w01/member/views.py
+++ w0609/w01/member/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 
 
+# 로그인부분 - get, post
+def login(request):
+    if request.method == 'GET':
+        return render(request,'member/login.html')
+    elif request.method == 'POST':
+        id = request.POST.get('id')
+        pw = request.POST.get('pw')
+        idsave = request.POST.get('idsave')
+        print("로그인 넘어온 데이터 : ",id,pw,idsave)
+        return render(request,'member/login.html')
+        
+
 # 회원가입부분
 def step03(request):
     return render(request,'member/step03.html')
