221e4288b5c0819b81af3df99035621fe87487f0
onulee <onulee@naver.com>
2025-06-09 12:53:57 +0900

    로그인 부분

--- w0609/w01/member/views.py
+++ w0609/w01/member/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from member.models import Member
 
 # 로그인부분 - get, post
 def login(request):
@@ -10,7 +10,15 @@ def login(request):
         pw = request.POST.get('pw')
         idsave = request.POST.get('idsave')
         print("로그인 넘어온 데이터 : ",id,pw,idsave)
-        return render(request,'member/login.html')
+        ## id,pw확인
+        msg = 0
+        try:
+            qs = Member.objects.get(id=id,pw=pw)
+            request.session['session_id'] = id #session id를 추가
+            msg = 1
+        except: print('데이터가 없습니다.')    
+        context = {'msg':msg}
+        return render(request,'member/login.html',context)
         
 
 # 회원가입부분
