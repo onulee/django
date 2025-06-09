233ca4e816c05281f4fc00e4ffa787fda5c407ab
onulee <onulee@naver.com>
2025-06-09 11:02:44 +0900

    step01,step02

---
+++ w0609/w01/member/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path,include
+from . import views
+
+app_name = 'member'
+urlpatterns = [
+    path('step01/', views.step01, name='step01'),
+    path('step02/', views.step02, name='step02'),
+]
--- w0609/w01/member/views.py
+++ w0609/w01/member/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+# 약관동의부분
+def step02(request):
+    return render(request,'member/step02.html')
+
+# 이메일인증부분
+def step01(request):
+    return render(request,'member/step01.html')
