14d720d859ede19ecce7174c67bebe1af1c37ead
onulee <onulee@naver.com>
2025-06-10 11:19:06 +0900

    board

--- w0609/w01/board/views.py
+++ w0609/w01/board/views.py
@@ -7,7 +7,9 @@ def list(request):
     # 페이지번호가 있어야 함.
     page = int(request.GET.get('page',1))
     qs = Board.objects.all().order_by('-ntchk','-bgroup','bstep')
+    # 하단넘버링부분
     paginator = Paginator(qs,10)    # 10개씩 분리해서 가져옴. 1,2,...10페이지생성
     list = paginator.get_page(page) # 1페이지 가져오기 -> 10개 게시글
+    # -----------
     context = {'list':list,'page':page}
     return render(request,'board/list.html',context)
