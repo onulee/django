eaec00abcd085208d8b6633d682a52bdba5e940a
onulee <onulee@naver.com>
2025-06-11 18:29:06 +0900

    데이터 전달방법1,4

--- w0609/w03/board/urls.py
+++ w0609/w03/board/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'board'
 urlpatterns = [
     path('list/', views.list, name='list'),
-    path('view/', views.view, name='view'),
+    path('view/', views.view, name='view'), # 1.form 데이터전송
+    path('view2/', views.view2, name='view2'), # 4.ajax 데이터전송
 ]
