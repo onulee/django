6cce0dd0d5b84395b6c41b2efb4dcae0f048c921
onulee <onulee@naver.com>
2025-06-11 12:00:56 +0900

    하단댓글 등록

--- w0609/w01/comment/views.py
+++ w0609/w01/comment/views.py
@@ -6,24 +6,22 @@ from member.models import Member
 from board.models import Board
 from comment.models import Comment
 
-def list(request):
-    # id = request.session['session_id'] # 로그인이 되어 있어야 하단댓글 가능
-    id = 'aaa'
+
+# 하단댓글저장
+def cwrite(request):
+    id = request.session['session_id'] # 로그인이 되어 있어야 하단댓글 가능
     member = Member.objects.get(id=id)
     bno = request.POST.get('bno',1)
     board = Board.objects.get(bno=bno)
     cpw = request.POST.get('cpw','')
     ccontent = request.POST.get('ccontent','')
-    print("넘어온 데이터 : ",member,board,cpw,ccontent)
+    print("넘어온 데이터 : ",cpw,ccontent)
     # QuerySet타입 -> list타입
-    qs = Comment.objects.create(board=board,member=member,cpw=cpw,
-                                 ccontent=ccontent)
-    
-    print('qs : ',qs) # ok
+    qs = Comment.objects.create(board=board,member=member,cpw=cpw,ccontent=ccontent)
     
     # filter 리스트타입으로 리턴
     list_qs = list(Comment.objects.filter(cno=qs.cno).values())
     print('list_qs : ',list_qs)
-    context = {'result':'success','comment':qs,'comment_list':list_qs}
+    context = {'result':'success','comment':list_qs}
     
     return JsonResponse(context)
