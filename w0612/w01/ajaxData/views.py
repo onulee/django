28415e9aa6ba8fad53c224bd991ea3a8f8f917bf
onulee <onulee@naver.com>
2025-06-12 16:12:00 +0900

    ajax list.html

--- w0612/w01/ajaxData/views.py
+++ w0612/w01/ajaxData/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from board.models import Board
+
 # form게시판 - get,post
 def blist(request):
-    context = {'result':'success'}
+    ### db게시글 전체가져오기
+    qs = Board.objects.all().order_by('-ntchk','-bgroup','bstep')
+    print('queryset타입 : ',qs)
+    ### json타입으로 변경
+    l_qs = list(qs.values())
+    print('리스트타입 : ',l_qs)
+    
+    context = {'result':'success','list':l_qs}
     return JsonResponse(context)
     
     
