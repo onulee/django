ba59568d3bb547682ed56c3cf819f56a0ec24605
onulee <onulee@naver.com>
2025-06-12 11:06:40 +0900

    form1,2,3

--- w0612/w03/board/urls.py
+++ w0612/w03/board/urls.py
@@ -3,5 +3,14 @@ from . import views
 
 app_name = 'board'
 urlpatterns = [
+    # ajax 전송형태
+    path('list3/', views.list3, name='list3'),
+    
+    # form 파라미터 형태
     path('list/', views.list, name='list'),
+    path('view/', views.view, name='view'),
+    
+    # form url api형태
+    path('list2/', views.list2, name='list2'),
+    path('view2/<int:bno>/', views.view2, name='view2'),
 ]
--- w0612/w03/board/views.py
+++ w0612/w03/board/views.py
@@ -2,6 +2,36 @@ from django.shortcuts import render,redirect
 from django.http import JsonResponse
 from board.models import Board
 
+def list3(request):
+    return render(request,'board/list3.html')
+
+#--------------------------------------------------
+
+def view2(request,bno):
+    print(bno)
+    qs = Board.objects.get(bno=bno)
+    context = {'board':qs}
+    return render(request,'board/view2.html',context)
+
+
+# from - get,post
+def list2(request):
+    if request.method == 'GET':
+        qs = Board.objects.all().order_by('-ntchk','-bgroup','bstep')
+        context = {'list':qs}
+        return render(request,'board/list2.html',context)
+    elif request.method == 'POST':
+        return render(request,'board/list2.html')
+
+#-------------------------------------------------
+
+def view(request):
+    bno = request.GET.get('bno')
+    print('넘어온 bno : ',bno)
+    qs = Board.objects.get(bno=bno)
+    context = {'board':qs}
+    return render(request,'board/view.html',context)
+
 # form게시판 - get,post
 def list(request):
     if request.method == 'GET':
