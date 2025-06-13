7f90f8a05f87f0d8f4ec56dbafff674e8192cb55
onulee <onulee@naver.com>
2025-06-13 13:16:04 +0900

    하단댓글 등록

--- w0613/w01/comment/views.py
+++ w0613/w01/comment/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from comment.models import Comment
+from member.models import Member
+from board.models import Board
 
 ## 하단등록 - Json타입리턴
 def cwrite(request):
     # html에서 디장고로 데이터 전달
     id = request.POST.get('id')
-    cpw = request.POST.get('cpw')
+    bno = request.POST.get('bno')
+    cpw = request.POST.get('cpw','')
     ccontent = request.POST.get('ccontent')
     print('넘어온 데이터 : ',id,cpw,ccontent)
+    # id -> member, bno -> board
+    member = Member.objects.get(id=id)
+    board = Board.objects.get(bno=bno)
     
-    context = {'result':'success'}
+    # db저장
+    qs = Comment.objects.create(member=member,board=board,cpw=cpw,ccontent=ccontent)
+    print(qs) #QuerySet타입
+    
+    # Json타입변경 - QuerySet List타입은 list타입으로 바로 변경
+    json_qs = list(Comment.objects.filter(cno=qs.cno).values())
+    print(json_qs)
+    context = {'result':'success','comment':json_qs}
     return JsonResponse(context)
 
 
