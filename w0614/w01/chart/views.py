25c09f3b7d7fadedd3cb4e4b458a188b7b94005a
onulee <onulee@naver.com>
2025-06-16 18:19:02 +0900

    차트그리기

--- w0614/w01/chart/views.py
+++ w0614/w01/chart/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from chart.models import TotalSales
 
 # 차트페이지 호출
 def chlist(request):
-    profit = [20, 15, 7, 25, 27, 30]
-    context = {'profit':profit}
+    profit = [20, 15, 7, 25, 27, 30]           # 타입 : List타입
+    qs = TotalSales.objects.filter(year=2025)
+    print('qs 기본구문 : ',qs)                  # 타입 : QuerySet List타입
+    print('list타입 구문 : ',list(qs.values())) # 타입 : List타입
+    context = {'profit':profit,'list':qs,'list_list':list(qs.values())}
     print("영업이익 : ",profit)
     return render(request,'chart/chlist.html',context)
