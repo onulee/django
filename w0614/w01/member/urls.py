5f47d8976d320334bd22019ee0fc3042bb9458d4
onulee <onulee@naver.com>
2025-06-16 11:02:13 +0900

    step01,02,03,04

---
+++ w0614/w01/member/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path,include
+from . import views
+
+app_name='member/'
+urlpatterns = [
+    path('step01/', views.step01, name='step01'),
+    path('step02/', views.step02, name='step02'),
+    path('step03/', views.step03, name='step03'),
+    path('step04/', views.step04, name='step04'),
+]
--- w0614/w01/member/views.py
+++ w0614/w01/member/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def step04(request):
+    return render(request,'member/step04.html')
+
+def step03(request):
+    return render(request,'member/step03.html')
+
+def step02(request):
+    return render(request,'member/step02.html')
+
+def step01(request):
+    return render(request,'member/step01.html')
