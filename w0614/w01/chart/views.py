efeda06106d05f0f216544383171c78e8b7c43b7
onulee <onulee@naver.com>
2025-06-16 18:02:36 +0900

    chart models.py

--- w0614/w01/chart/views.py
+++ w0614/w01/chart/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # 차트페이지 호출
 def chlist(request):
-    profit = [12, 19, 3, 5, 2, 3]
+    profit = [20, 15, 7, 25, 27, 30]
     context = {'profit':profit}
+    print("영업이익 : ",profit)
     return render(request,'chart/chlist.html',context)
