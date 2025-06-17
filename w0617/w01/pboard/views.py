9bdb4ee637493aac44ec15094e8ce1bf33d7acce
onulee <onulee@naver.com>
2025-06-17 18:24:57 +0900

    공공데이터 리스트,뷰페이지

--- w0617/w01/pboard/views.py
+++ w0617/w01/pboard/views.py
@@ -2,13 +2,19 @@ from django.shortcuts import render
 import requests
 import json
 
-# 공공데이터 상세보기
+# 공공데이터 상세보기 - 공공데이터 다시 호출
 def view(request,galContentId):
     print('넘어온 galContentId : ',galContentId)
     # 공공데이터 호출
     dlist = publicData()
-    print('데이터 호출 : ',dlist)
-    return render(request,'pboard/view.html')
+    dData = {}
+    for d in dlist:
+        if d['galContentId'] == str(galContentId): # 타입확인
+            dData = d
+            break
+        
+    context = {'dData':dData}
+    return render(request,'pboard/view.html',context)
 
 
 # 공공데이터 리스트
@@ -24,12 +30,12 @@ def publicData():
     url = f'http://apis.data.go.kr/B551011/PhotoGalleryService1/galleryList1?serviceKey={public_key}&numOfRows=10&pageNo={pageNo}&MobileOS=ETC&MobileApp=AppTest&arrange=A&_type=json'
     # 웹스크래핑 requests
     response = requests.get(url)
-    print("공공데이터 : ",response.text)  # str타입
+    #print("공공데이터 : ",response.text)  # str타입
     
     # 문자열 -> json타입으로 변경
     json_data = json.loads(response.text)
     dlist = json_data['response']['body']['items']['item']
-    print('json데이터 : ',json_data['response']['body']['items']['item'])     # json타입
-    print('json데이터 1개 : ',json_data['response']['body']['items']['item'][0])
+    # print('json데이터 : ',json_data['response']['body']['items']['item'])     # json타입
+    # print('json데이터 1개 : ',json_data['response']['body']['items']['item'][0])
     
     return dlist    
