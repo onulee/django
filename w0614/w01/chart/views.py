5358e15ea181730964cc3085e6d855135ec39de2
onulee <onulee@naver.com>
2025-06-17 13:05:50 +0900

    엔터키 차트

--- w0614/w01/chart/views.py
+++ w0614/w01/chart/views.py
@@ -8,8 +8,10 @@ def chlist2(request):
 
 # ajax으로 json타입으로 리턴
 def chajax(request):
+    aYear = request.POST.get('aYear')
+    print('넘어온 aYear : ',aYear)
     # db불러오기 - 하단댓글때 qs list타입변경
-    qs = TotalSales.objects.filter(year=2024).order_by('-totalSales')[:5]
+    qs = TotalSales.objects.filter(year=aYear)
     print('qs 기본구문 : ',qs)                  # 타입 : QuerySet List타입
     print('list타입 구문 : ',list(qs.values())) # 타입 : List타입
     # json타입으로 변경
