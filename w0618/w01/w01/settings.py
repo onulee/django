9c31b505995135b043d0eb6fffffac917a0b30ef
onulee <onulee@naver.com>
2025-06-18 12:19:28 +0900

    영화데이터

--- w0618/w01/w01/settings.py
+++ w0618/w01/w01/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'home',
     'member',
     'board',
