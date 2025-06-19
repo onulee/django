da46771e0364fe34d5b83f75d251a6e42f679835
onulee <onulee@naver.com>
2025-06-19 13:17:19 +0900

    shop login

--- shopProject/shop/member/urls.py
+++ shopProject/shop/member/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name='member'
 urlpatterns = [
     path('login/', views.login,name='login'),
+    path('step01/', views.step01,name='step01'),
 ]
--- shopProject/shop/member/views.py
+++ shopProject/shop/member/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from member.models import Member
 
+# get:로그인페이지, post:로그인확인
 def login(request):
-    return render(request,'member/login.html')
+    if request.method == 'GET':
+        return render(request,'member/login.html')
+    elif request.method == 'POST':
+        id = request.POST.get('id')
+        pw = request.POST.get('pw')
+        return render(request,'member/login.html')
+        
+
+# 회원가입01
+def step01(request):
+    return render(request,'member/step01.html')
