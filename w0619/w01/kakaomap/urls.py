d48041a74cb735fa01a974b33e15e50ebb3898bd
onulee <onulee@naver.com>
2025-06-19 12:06:10 +0900

    컴퍼넌트

--- w0619/w01/kakaomap/urls.py
+++ w0619/w01/kakaomap/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name='kakaomap'
 urlpatterns = [
     path('list/', views.list, name='list'),
+    path('list2/', views.list2, name='list2'),
 ]
--- w0619/w01/kakaomap/views.py
+++ w0619/w01/kakaomap/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
+def list2(request):
+    return render(request,'kakaomap/list2.html')
+
 def list(request):
     return render(request,'kakaomap/list.html')
