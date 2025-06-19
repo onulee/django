61ff2d4244ab9962924114cef44800b20ccab386
onulee <onulee@naver.com>
2025-06-19 16:23:08 +0900

    로그인 부분

--- shopProject/shop/member/views.py
+++ shopProject/shop/member/views.py
@@ -1,15 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from member.models import Member
 
 # get:로그인페이지, post:로그인확인
 def login(request):
+    ## 쿠키정보 가져오기
+    cook_id = request.COOKIES.get('cook_id','')
+    context = {'cook_id':cook_id}
     if request.method == 'GET':
-        return render(request,'member/login.html')
+        return render(request,'member/login.html',context)
     elif request.method == 'POST':
+        
         id = request.POST.get('id')
         pw = request.POST.get('pw')
-        return render(request,'member/login.html')
+        idsave = request.POST.get('idsave')
+        print('넘어온 id,pw,idsave : ',id,pw,idsave)
+        ## db확인
+        qs = Member.objects.filter(id=id,pw=pw) # None
+        print('qs : ',qs)
+        if qs:
+            msg = 1
+            request.session['session_id'] = id
+            request.session['session_name'] = qs[0].name
+        else:
+            msg = 0    
+        context['msg'] = msg
+        response = render(request,'member/login.html',context)
+        if idsave:
+            response.set_cookie('cook_id',id,max_age=60*60*24)
+        else:
+            response.delete_cookie('cook_id')
+        return response
         
+# 로그아웃
+def logout(request):
+    ### 섹션 모두삭제
+    request.session.clear()
+    msg = 2
+    context = {'msg':msg}
+    return render(request,'member/login.html',context)
 
 # 회원가입01
 def step01(request):
