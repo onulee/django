14f9ca5e05c1e52f529e058bc075cd7990254d1d
onulee <onulee@naver.com>
2025-06-20 11:19:34 +0900

    게시판 글쓰기

--- shopProject/shop/customer/urls.py
+++ shopProject/shop/customer/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name='customer'
 urlpatterns = [
     path('list/', views.list,name='list'),
+    path('view/<int:bno>/', views.view,name='view'),
+    path('write/', views.write,name='write'),
 ]
--- shopProject/shop/customer/views.py
+++ shopProject/shop/customer/views.py
@@ -1,5 +1,35 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from customer.models import Customer
+
+# 게시글 상세보기
+def view(request):
+    context = {'list':''}
+    return render(request,'customer/list.html',context)
+
+# 게시글 쓰기 - get:글쓰기페이지, post:글쓰기저장
+def write(request):
+    if request.method == 'GET':
+        return render(request,'customer/write.html')
+    elif request.method == 'POST':
+        return render(request,'customer/write.html')
+        
 
 # 게시판리스트
 def list(request):
-    return render(request,'customer/list.html')
+    # 요청하는 page번호 가져오기, str타입 -> int타입
+    page = int(request.GET.get('page',1))
+    # db에서 데이터 가져오기
+    qs = Customer.objects.all().order_by('-ntchk','-bgroup','bstep')
+    # 10개단위로 qs로 분리시킴
+    paginator = Paginator(qs,10)
+    
+    # 가져올 페이지 선택
+    customerList = paginator.get_page(page)
+    print('-----------------')
+    print(customerList)
+    print('-----------------')
+    
+    # 게시글 10개, 현재페이지 보냄
+    context = {'list':customerList,'page':page}
+    return render(request,'customer/list.html',context)
